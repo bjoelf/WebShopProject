7f93d32f3c8a892d0b88783bc11789cafbbef2f3
Björn Elfvin <bjorn@elfvin.se>
2021-06-16 19:51:07 +0200

    Add project files.

---
+++ WebShopProjectApp/Customers/Customer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebShopProjectApp.Customers
+{
+    public class Customer
+    {
+    }
+}
