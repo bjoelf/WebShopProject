865b3b4c4113722af7e35f56142717aeaef8e727
Björn Elfvin <bjorn@elfvin.se>
2021-06-29 16:50:02 +0200

    Working on Order edit stuff

---
+++ WebShopProjectApp/ViewModels/EditOrder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using WebShopProjectApp.Users;
+
+namespace WebShopProjectApp.ViewModels
+{
+    public class EditOrder
+    {
+        public int Id { set; get; }
+        public CreateOrder CreateOrder { set; get; }
+        public User Customer { set; get; }
+    }
+}
